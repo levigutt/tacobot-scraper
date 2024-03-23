b11173d839b29ce246cec2ea4e8fe80fdd3bca35
levi <levinj@pm.me>
2024-03-23 10:01:15 +0100

    improve

---
+++ README.md
@@ -0,0 +1,3 @@
+```sh
+./script.pl > out
+```
--- script.pl
+++ script.pl
@@ -3,20 +3,26 @@ use strict;
 use Firefox::Marionette;
 use Data::Printer;
 
+our %profiles;
+sub done
+{
+    print for keys %profiles;
+}
+
 # ensure output even when closed with Ctrl+C or kill
 $SIG{TERM} = \&done;
 $SIG{INT} = \&done;
+
 
 my $m = Firefox::Marionette->new(visible => 1);
 $m->go('https://tacobot.tf/leaderboard');
 sleep 3; # wait for page to load
 
 my $expected_count = $m->find_selector('[aria-rowcount]')->attribute('aria-rowcount');
-$expected_count--; # dont count header
+$expected_count--; # dont count header row
 warn "expected count: $expected_count\n";
 
 my @elems;
-our %profiles;
 while(1)
 {
     # get profiles and add to list
@@ -34,7 +40,3 @@ while(1)
 
 done();
 
-sub done
-{
-    print for keys %profiles;
-}
